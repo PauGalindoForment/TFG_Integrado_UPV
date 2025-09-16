53f266639e9bdbbc8cf81b41ed646910335f79a9
PauGalindoForment <paugalindo22@gmail.com>
2025-09-16 23:07:51 +0200

    Create CampoGolfIndoor.py

---
+++ BBDD_Unificada/CampoGolfIndoor.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Cargar archivos
+ruta_escenas = "C:/NAS_PAU/TFG/Places365/escenas_places365_con_indoor_final.csv"
+ruta_publicaciones = "C:/NAS_PAU/TFG/BBDD_Unificada/DatosPublicacionesPGALIV.xlsx"
+
+df_escenas = pd.read_csv(ruta_escenas, sep=';') 
+df_publicaciones = pd.read_excel(ruta_publicaciones)
+
+# Unir por 'imagen'
+df_final = df_publicaciones.merge(
+    df_escenas[['nombre_archivo', 'campo_golf', 'indoor']],
+    how='left',
+    on='nombre_archivo'
+)
+
+# Guardar resultado
+ruta_salida = "C:/NAS_PAU/TFG/BBDD_Unificada/DatosPublicacionesPGALIV.xlsx"
+df_final.to_excel(ruta_salida, index=False)
+
+print(f"Archivo guardado en: {ruta_salida}")
