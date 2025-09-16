b74dc04306c502c6cf27a8e445178212666bae13
PauGalindoForment <paugalindo22@gmail.com>
2025-09-16 23:16:49 +0200

    Update UnirEmocionesRostros.py

--- BBDD_Unificada/UnirEmocionesRostros.py
+++ BBDD_Unificada/UnirEmocionesRostros.py
@@ -1,21 +1,22 @@
 import pandas as pd
 
-# === Cargar archivos ===
+# Cargar archivos
 ruta_escenas = "C:/NAS_PAU/TFG/DeepFace/emociones_por_imagen_final.csv"
 ruta_publicaciones = "C:/NAS_PAU/TFG/BBDD_Unificada/DatosPublicacionesPGALIV.xlsx"
 
-df_escenas = pd.read_csv(ruta_escenas, sep=';')  # Usa sep adecuado si es necesario
+df_escenas = pd.read_csv(ruta_escenas, sep=';')  
 df_publicaciones = pd.read_excel(ruta_publicaciones)
 
-# === Unir por 'imagen' ===
+# Unir por 'imagen'
 df_final = df_publicaciones.merge(
     df_escenas[['nombre_archivo', 'Rostros', 'Emocion']],
     how='left',
     on='nombre_archivo'
 )
 
-# === Guardar resultado ===
+# Guardar resultado
 ruta_salida = "C:/NAS_PAU/TFG/BBDD_Unificada/DatosPublicacionesPGALIV.xlsx"
 df_final.to_excel(ruta_salida, index=False)
 
 print(f"Archivo guardado en: {ruta_salida}")
+
